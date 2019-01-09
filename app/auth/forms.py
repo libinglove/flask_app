f3f1cdda9db5e22d2eca79480f407b084c1c8bdb
libinglove <18310275531@163.com>
2019-01-09 16:27:23 +0800

    reset password

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -45,7 +45,20 @@ class ChangePasswordForm(FlaskForm):
 
 
 
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField('Email',validators=[Required(),Length(1,63),Email()])
+    submit = SubmitField('重设密码')
 
+
+class PasswordResetForm(FlaskForm):
+    email = StringField('Email',validators=[Required(),Length(1,64),Email()])
+    password = PasswordField('新密码',validators=[Required(),EqualTo('password2',message='密码必须匹配')])
+    password2 = PasswordField('确认密码',validators=[Required()])
+    submit = SubmitField('重设密码')
+
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first() is None :
+            raise ValidationError('错误的邮箱地址')
 
 
 
